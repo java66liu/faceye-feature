a6300123f22df667399eab99070eb96c1044b14f
haipenge@gmail.com <haipenge@gmail.com>
2014-06-26 09:40:10 +0800

    修复SQL注入漏洞

--- src/main/java/com/faceye/feature/repository/DynamicSQLBuilder.java
+++ src/main/java/com/faceye/feature/repository/DynamicSQLBuilder.java
@@ -1,6 +1,8 @@
 package com.faceye.feature.repository;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections.MapUtils;
@@ -23,8 +25,10 @@ public class DynamicSQLBuilder {
 	 * haipenge@gmail.com
 	 * 2014年6月25日
 	 */
-	public static String builder(Map<String, SearchFilter> filters, String sql) {
+	public static SQLBuilderEntity builder(Map<String, SearchFilter> filters, String sql) {
 		StringBuilder res = new StringBuilder(sql);
+		SQLBuilderEntity entity=new SQLBuilderEntity();
+		List<Object> paramValues=new ArrayList<Object>();
 		res=addWhere2SQL(res);
 		if (MapUtils.isNotEmpty(filters)) {
 			Iterator<SearchFilter> it = filters.values().iterator();
@@ -32,59 +36,68 @@ public class DynamicSQLBuilder {
 				SearchFilter filter = it.next();
 				String key=filter.fieldName;
 				Object value=filter.value;
+			
 				switch (filter.operator) {
 				case EQ:
 					res.append(" and ");
 					res.append(key);
 					res.append("=");
-					res.append("'");
-					res.append(value);
-					res.append("'");
+//					res.append("'");
+					res.append("?");
+//					res.append("'");
+					paramValues.add(value);
 					break;
 				case LIKE:
 					res.append(" and ");
 					res.append(key);
 					res.append(" like ");
-					res.append("'%");
-					res.append(value);
-					res.append("%'");
+//					res.append("'%");
+					res.append("?");
+//					res.append("%'");
+					paramValues.add("%"+value+"%");
 					break;
 				case GT:
 					res.append(" and ");
 					res.append(key);
 					res.append(">");
-					res.append("'");
-					res.append(value);
-					res.append("'");
+//					res.append("'");
+					res.append("?");
+//					res.append("'");
+					paramValues.add(value);
 					break;
 				case LT:
 					res.append(" and ");
 					res.append(key);
 					res.append("<");
-					res.append("'");
-					res.append(value);
-					res.append("'");
+//					res.append("'");
+					res.append("?");
+//					res.append("'");
+					paramValues.add(value);
 					break;
 				case GTE:
 					res.append(" and ");
 					res.append(key);
 					res.append(">=");
-					res.append("'");
-					res.append(value);
-					res.append("'");
+//					res.append("'");
+					res.append("?");
+//					res.append("'");
+					paramValues.add(value);
 					break;
 				case LTE:
 					res.append(" and ");
 					res.append(key);
 					res.append("<=");
-					res.append("'");
-					res.append(value);
-					res.append("'");
+//					res.append("'");
+					res.append("?");
+					paramValues.add(value);
+//					res.append("'");
 				default:
 				}
 			}
 		}
-		return res.toString();
+		entity.setSql(res.toString());
+		entity.setParamValues(paramValues);
+		return entity;
 	}
 	/**
 	 * 判断SQL中是否有where语句
