ef60036ce19e4311f87af2b686485edf7b1bb6b9
haipenge@gmail.com <haipenge@gmail.com>
2014-08-16 20:38:41 +0800

    增加mongo支持

--- src/template/source/main/java/Document.java
+++ src/template/source/main/java/Document.java
@@ -3,12 +3,9 @@ package com.faceye.component.@component.name@.doc;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
 import javax.persistence.Id;
-import javax.persistence.Table;
+import org.springframework.data.mongodb.core.mapping.Document;
 /**
  * @entity.name@ ORM 实体
  * 数据库表:@component.name@_@entity.config.name@
--- src/template/source/main/java/DocumentServiceImpl.java
+++ src/template/source/main/java/DocumentServiceImpl.java
@@ -15,7 +15,7 @@ import com.faceye.component.@component.name@.doc.@entity.name@;
 
 import com.faceye.component.@component.name@.repository.mongo.@entity.name@Repository;
 import com.faceye.component.@component.name@.service.@entity.name@Service;
-import com.faceye.feature.service.impl.BaseServiceImpl;
+import com.faceye.feature.service.impl.BaseMongoServiceImpl;
 import com.faceye.feature.util.ServiceException;
 
 @Service
