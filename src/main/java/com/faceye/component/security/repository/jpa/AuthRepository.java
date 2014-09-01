345e84e6129ab5beb92c3009f1ace61fed43a397
haipenge@gmail.com <haipenge@gmail.com>
2014-09-01 17:53:27 +0800

    删除不需要的代码

--- src/main/java/com/faceye/component/security/repository/jpa/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.faceye.component.security.repository.jpa;
-
-import java.util.List;
-
-import org.springframework.stereotype.Repository;
-
-public interface AuthRepository {
-   public List<?> getUsers();
-}
